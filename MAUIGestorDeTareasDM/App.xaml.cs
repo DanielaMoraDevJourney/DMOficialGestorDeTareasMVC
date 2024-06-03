ee1549674c6b21cce3ddf6857de654e865ddb544
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-02 20:46:30 -0500

    api

---
+++ MAUIGestorDeTareasDM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIGestorDeTareasDM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MAUIGestorDeTareasDM/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIGestorDeTareasDM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
